59569de328fb090a1d43b70ce31c05811f2df8b9
Rafayel Khachatryan <rafayel.khachatryan1991@gmail.com>
2021-09-03 20:21:58 +0400

    Categories table, Localization, login, register.

---
+++ config/language.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'en' => [
+        'display' => 'English',
+        'flag-icon' => 'en'
+    ],
+    'hy' => [
+        'display' => 'Հայերեն',
+        'flag-icon' => 'hy'
+    ],
+    'ru' => [
+        'display' => 'Русский',
+        'flag-icon' => 'ru'
+    ],
+];

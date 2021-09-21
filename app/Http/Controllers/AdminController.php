7232c7ba0a81f2a41db11d151a4c934ece4596fb
Vahan <vahan.musayelyan@mail.ru>
2021-09-21 23:39:26 +0400

    Other attributes, blogs create, edit, update, delete.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
+use App\Models\BlogTrans;
 use App\Models\HouseAttr;
 use App\Models\HouseAttrTrans;
+use App\Models\HouseOtherAttrCat;
+use App\Models\HouseOtherAttrCatTrans;
 use App\Models\Room;
 use App\Models\RoomTrans;
 use App\Scopes\LanguageScope;
@@ -137,8 +141,234 @@ class AdminController extends Controller
 
     public function attributeDelete($id)
     {
-        HouseAttrTrans::withoutGlobalScope(LanguageScope::class)->where("attr_id",$id)->delete();
+        HouseAttrTrans::withoutGlobalScope(LanguageScope::class)->where("attr_id", $id)->delete();
+        HouseAttr::withoutGlobalScope(LanguageScope::class)->where("id", $id)->delete();
 
         return redirect("/dashboard/attributes");
     }
+
+
+    public function otherAttributes()
+    {
+        $attrs = HouseOtherAttrCat::withoutGlobalScope(LanguageScope::class)->get();
+        $parents = HouseOtherAttrCat::withoutGlobalScope(LanguageScope::class)->where("parent_id", null)->get();
+
+        $attrsHy = HouseOtherAttrCatTrans::withoutGlobalScope(LanguageScope::class)->where("lang", "hy")->orderBy("attr_cat_id", "asc")->get();
+        $attrsEn = HouseOtherAttrCatTrans::withoutGlobalScope(LanguageScope::class)->where("lang", "en")->orderBy("attr_cat_id", "asc")->get();
+        $attrsRu = HouseOtherAttrCatTrans::withoutGlobalScope(LanguageScope::class)->where("lang", "ru")->orderBy("attr_cat_id", "asc")->get();
+
+
+        return view("admin.other_attribute")->with([
+            "attrs" => $attrs,
+            "attrsHy" => $attrsHy,
+            "attrsEn" => $attrsEn,
+            "attrsRu" => $attrsRu,
+            "parents" => $parents,
+        ]);
+    }
+
+    public function addOtherAttribute(Request $request)
+    {
+        $attr = new HouseOtherAttrCat();
+        if ($request->parent_id != null && $request->parent_id != "") {
+            $attr->type = $request->type;
+            $attr->value = $request->value;
+        }
+        $attr->name = $request->attr_hy;
+        $attr->parent_id = $request->parent_id;
+
+        $attr->save();
+
+        HouseOtherAttrCatTrans::insert([
+            "lang" => "hy",
+            "attr_cat_id" => $attr->id,
+            "name" => $request->attr_hy
+        ]);
+
+        return redirect()->back();
+    }
+
+    public function otherAttributeEdit($id)
+    {
+        $parents = HouseOtherAttrCat::withoutGlobalScope(LanguageScope::class)->where("parent_id", null)->get();
+        $attr = HouseOtherAttrCat::withoutGlobalScope(LanguageScope::class)->where("id", $id)->get();
+
+        return view("admin.other_attr_edit")->with([
+            "attr" => $attr,
+            "parents" => $parents,
+        ]);
+    }
+
+    public function otherAttributeUpdate(Request $request)
+    {
+        $attr = new HouseOtherAttrCat();
+        if ($request->parent_id != null && $request->parent_id != "") {
+            $attr->type = $request->type;
+            $attr->value = $request->value;
+        }
+        $attr->name = $request->attr_hy;
+        $attr->parent_id = $request->parent_id;
+
+        $attr->save();
+
+        HouseOtherAttrCatTrans::insert([
+            "lang" => "hy",
+            "attr_cat_id" => $attr->id,
+            "name" => $request->attr_hy
+        ]);
+
+        return redirect()->back();
+    }
+
+    public function otherAttributeDelete($id)
+    {
+
+        HouseOtherAttrCatTrans::withoutGlobalScope(LanguageScope::class)->where("attr_cat_id", $id)->delete();
+        HouseOtherAttrCat::withoutGlobalScope(LanguageScope::class)->where("id", $id)->delete();
+
+        return redirect("/dashboard/other-attributes");
+    }
+
+    public function blogs()
+    {
+
+        $blogs = Blog::all();
+
+        return view("admin.blogs")->with([
+            "blogs" => $blogs
+        ]);
+    }
+
+    public function addBlog(Request $request)
+    {
+
+        if (isset($request->img_first)) {
+            $imgFirst = $request->img_first;
+            $imageNameFirst = time() . '.' . $imgFirst->extension();
+            $imgFirst->move(public_path('img/blogs'), $imageNameFirst);
+        } else {
+            $imageNameFirst = "";
+        }
+
+        if (isset($request->img_second)) {
+            $imgSecond = $request->img_second;
+            $imageNameSecond = time() . '.' . $imgSecond->extension();
+            $imgSecond->move(public_path('img/blogs'), $imageNameSecond);
+        } else {
+            $imageNameSecond = "";
+        }
+
+        $blog = new Blog();
+        $blog->theme = $request->theme;
+        $blog->tags = $request->tags;
+        $blog->header = $request->header;
+        $blog->content_first = $request->content_first;
+        $blog->content_second = $request->content_second;
+        $blog->img_first = $imageNameFirst;
+        $blog->img_second = $imageNameSecond;
+        $blog->save();
+
+        BlogTrans::insert([
+            "blog_id" => $blog->id,
+            "header" => $request->header,
+            "content_first" => $request->content_first,
+            "content_second" => $request->content_second,
+            "theme" => $request->theme,
+            "tags" => $request->tags,
+            "lang" => "hy"
+        ]);
+
+        return redirect()->back();
+    }
+
+    public function blogEdit($id)
+    {
+        $blog = Blog::where("id", $id)->first();
+        $blogHy = BlogTrans::withoutGlobalScope(LanguageScope::class)->where("blog_id", $id)->where("lang", "hy")->first();
+        $blogEn = BlogTrans::withoutGlobalScope(LanguageScope::class)->where("blog_id", $id)->where("lang", "en")->first();
+        $blogRu = BlogTrans::withoutGlobalScope(LanguageScope::class)->where("blog_id", $id)->where("lang", "ru")->first();
+
+        return view("admin.blog_edit")->with([
+            "blog" => $blog,
+            "blogHy" => $blogHy,
+            "blogEn" => $blogEn,
+            "blogRu" => $blogRu,
+        ]);
+    }
+
+    public function blogUpdate(Request $request)
+    {
+        $blogUpdated = Blog::where("id", $request->number)->first();
+
+        $blog = Blog::find($request->number);
+
+        if (isset($request->img_first)) {
+            if (File::exists(public_path('img/blogs/' . $blogUpdated->image_first))) {
+                File::delete(public_path('img/blogs/' . $blogUpdated->image_first));
+            }
+            $imgFirst = $request->img_first;
+            $imageNameFirst = time() . '0.' . $imgFirst->extension();
+            $imgFirst->move(public_path('img/blogs'), $imageNameFirst);
+        } else {
+            $imageNameFirst = $blogUpdated->img_first;
+        }
+
+        if (isset($request->img_second)) {
+            if (File::exists(public_path('img/blogs/' . $blogUpdated->image_second))) {
+                File::delete(public_path('img/blogs/' . $blogUpdated->image_second));
+            }
+            $imgSecond = $request->img_second;
+            $imageNameSecond = time() . '1.' . $imgSecond->extension();
+            $imgSecond->move(public_path('img/blogs'), $imageNameSecond);
+        } else {
+            $imageNameSecond = $blogUpdated->img_second;
+        }
+
+
+        $blog->theme = $request->theme;
+        $blog->tags = $request->tags;
+        $blog->header = $request->header_hy;
+        $blog->content_first = $request->content_first_hy;
+        $blog->content_second = $request->content_second_hy;
+        $blog->img_first = $imageNameFirst;
+        $blog->img_second = $imageNameSecond;
+        $blog->save();
+
+        BlogTrans::withoutGlobalScope(LanguageScope::class)->where("blog_id", $request->number)->delete();
+
+        foreach ($this->lang as $lang) {
+            BlogTrans::insert([
+                "blog_id" => $request->number,
+                "header" => $request->{"header_$lang"},
+                "content_first" => $request->{"content_first_$lang"},
+                "content_second" => $request->{"content_second_$lang"},
+                "theme" => $request->theme,
+                "tags" => $request->tags,
+                "lang" => $lang
+            ]);
+        }
+
+        return redirect("/dashboard/blogs");
+    }
+
+    public function blogDelete($id)
+    {
+        $blog = Blog::where("id", $id)->first();
+        if (File::exists(public_path('img/blogs/' . $blog->image_first))) {
+            File::delete(public_path('img/blogs/' . $blog->image_first));
+        }
+        if (File::exists(public_path('img/blogs/' . $blog->image_second))) {
+            File::delete(public_path('img/blogs/' . $blog->image_second));
+        }
+
+        $result = BlogTrans::withoutGlobalScope(LanguageScope::class)->where("blog_id", $id)->delete();
+
+        if ($result) {
+            Blog::where("id", $id)->delete();
+        }
+
+
+        return redirect("/dashboard/blogs");
+    }
+>>>>>>> f1a95b3... Other attributes, blogs create, edit, update, delete.
 }
